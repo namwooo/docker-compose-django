266e34c9271440eb80665991509b01f5a092116f
dev_youngchan <youngchan.kim@timeforpoetry.com>
2018-11-18 16:10:13 +0900

    Create custom user model and permission model

--- handys/users/admin.py
+++ handys/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import PermissionLevel1, PermissionLevel2, User
+
+
+@admin.register(User)
+class ParentPermissionAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'created_at', 'updated_at']
+    list_filter = ['created_at']
+
+
+@admin.register(PermissionLevel1)
+class ParentPermissionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at', 'is_available']
+    list_filter = ['created_at', 'name']
+
+
+@admin.register(PermissionLevel2)
+class ParentPermissionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at', 'is_available']
+    list_filter = ['created_at', 'name']
--- handys/users/models.py
+++ handys/users/models.py
@@ -1,3 +1,94 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password, first_name, last_name):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, first_name, last_name):
+        user = self.create_user(email=email, password=password,
+                                first_name=first_name, last_name=last_name)
+
+        user.is_active = True
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = MyUserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    @property
+    def token(self):
+        return Token.objects.get_or_create(user=self)[0].key
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        full_name = f'{self.first_name} {self.last_name}'
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class BasePermission(models.Model):
+    name = models.CharField('name', max_length=16)
+
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PermissionLevel1(BasePermission):
+    pass
+
+
+class PermissionLevel2(BasePermission):
+    parents = models.ManyToManyField(PermissionLevel1, blank=True)
